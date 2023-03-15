962722ba14d98706f7db2761f2d0e459c7583f0c
Varshini <varshini.sanakr@cdw.com>
2023-03-15 15:34:20 +0530

    Made few changes

--- Exercise-10/js/Section3.js
+++ Exercise-10/js/Section3.js
@@ -11,30 +11,14 @@ function displayDate()
     let formatted_date = datetime.getDate() + "/" + (datetime.getMonth()+1) + "/" + datetime.getFullYear();
     console.log("Today's date: "+formatted_date);
 }
-function displayTemp(celsius)
-{
-    const fahrenheit = (celsius * 1.8) + 32;
-    return fahrenheit;
-}
-function average(...arr)
-{
-    var sum=0;
-    for(let num of arr)
-    {
-        sum+=num;
-    }
-    return sum/arr.length;
-}
-function reverseString(str)
-{
-    return str.split('').reverse().join('');
-}
 displayNumbers();
 displayDate();
-let Celsius=40;
+let Celsius=45;
+displayTemp = celsius => (celsius * 1.8) + 32;
 let fahrenheit = displayTemp(Celsius);
 console.log(`${Celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`);
 const numbers = [3,5,6,7,8,1,10,34,9,2];
-var result = average(...numbers);
+var result = numbers.reduce((prevVal, currVal) => prevVal+currVal, 0)/numbers.length;
 console.log(`Average of the given numbers: ${result}`);
+reverseString = (str) => str.split('').reverse().join('');
 console.log(reverseString("VARSHINI"));
