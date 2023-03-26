46fcc0cd85038c1849788bc541691b4a6a35e052
Varshini <varshini.sanakr@cdw.com>
2023-03-27 02:31:27 +0530

    Added new file

---
+++ Exercise-20/js/myscript.js
@@ -0,0 +1,8 @@
+// Sidebar functions
+function openNav() {
+	document.getElementById("transparency-effect").style.display = "flex";
+}
+
+function closeNav() {
+	document.getElementById("transparency-effect").style.display = "none";
+}
