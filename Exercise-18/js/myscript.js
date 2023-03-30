3bbfd30e55617ed2b8b2cc5fca1df0d2a955feff
Varshini <varshini.sanakr@cdw.com>
2023-03-31 01:25:03 +0530

    Added comments

--- Exercise-18/js/myscript.js
+++ Exercise-18/js/myscript.js
@@ -8,28 +8,38 @@ $.get('https://mocki.io/v1/8c9b378b-d248-4203-93b0-b8e7659ac346', function(poste
     }
 );
 $.get('https://mocki.io/v1/4da47fc5-bbf3-4e41-b35f-c88a584bc4b0', function(videoContents) {
-    let left_container = $('.left-container');
+    let video_section = $('#video-section');
     let video = $('<video>').attr('src', videoContents.videoUrl).attr('controls', true).attr('poster', 'https://www.slashcam.de/images/news/sprite_fright1-16857_PIC1.jpg');
-    left_container.append(video);
+    video_section.append(video);
     let heading = $('<h3>').text(videoContents.title);
-    left_container.append(heading);
+    video_section.append(heading);
     let description = $('<p>').text(videoContents.description);
-    left_container.append(description);
+    video_section.append(description);
     let h_ruler = $('<div>').addClass('h-ruler');
-    left_container.append(h_ruler);
+    video_section.append(h_ruler);
+
+    let comment_container = $('div').addClass('comment-section');
     let commentsHeading = $('<h4>').text('Comments');
-    left_container.append(commentsHeading);
+    comment_container.append(commentsHeading);
+
+    let comment_section = $('.left-container');
 
     for (let comment of videoContents.comments) {
-        let comment_section = $('<div>').addClass('comments');
-        left_container.append(comment_section);
+        let comments = $('<div>').addClass('comments');
+
         let comment_image = $('<img>').attr('src', comment.image);
-        comment_section.append(comment_image);
+        comments.append(comment_image);
+
         let comment_information = $('<div>').addClass('comments-info');
-        comment_section.append(comment_information);
+
         let comment_name = $('<div>').addClass('name').text(comment.name);
         comment_information.append(comment_name);
+
         let comment_description = $('<div>').addClass('description').text(comment.comment);
         comment_information.append(comment_description);
+
+        comments.append(comment_information);
+        comment_container.append(comments);
+        comment_section.append(comment_container);
     }
 });
